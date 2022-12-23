39e7585bc45c5dbb29409975c552db62b30d4e20
ElenaV5 <elenavnev@gmail.com>
2022-12-23 20:37:30 +0500

    GenericTypes with deep 2

--- src/ru/synergy/Main.java
+++ src/ru/synergy/Main.java
@@ -1,6 +1,7 @@
 package ru.synergy;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
@@ -44,7 +45,7 @@ public class Main {
         */
 
         //Generics - возможность добавлять типы как параметры (к чему угодно), получаются составные типы, которые можно переиспользовать
-        //Основной тип<Тип параметров>
+        //ОсновнойТип<ТипПараметр>
         ArrayList<Integer> numbers = new ArrayList(); //сохраняет объекты
         for(int i = 0; i < 10; i++){ //сохранять в коллекцию числа 10, 20, 30... 100
             numbers.add(i*10);
@@ -56,5 +57,38 @@ public class Main {
             sum = sum + o; //здесь не нужно уточнаять, что o - Integer
         }
         System.out.println(sum); //450
+
+        //ОсновнойТип<ТипПараметр1, ТипПараметр2, ТипПараметр3>
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        map.put(7, "Hi");
+        map.put(15, "Hello");
+
+        ArrayList<String> listHello = new ArrayList();
+        listHello.add("Привет");
+        listHello.add("Hi");
+
+        ArrayList<String> listBye = new ArrayList();
+        listBye.add("Пока");
+        listBye.add("Good Bye");
+
+        //мы можем один лист вложить в уже созданный лист
+        ArrayList<ArrayList<String>> list = new ArrayList();
+        list.add(listHello);
+        list.add(listBye);
+
+        for(ArrayList<String> spisok : list){
+            for(String s : spisok){
+                System.out.println(s);
+            }
+        }
     }
 }
+
+/* Как работаю Generics:
+*    то что делаем мы в коде                                    то во что преобразует компилятор
+* ArrayList<Integer> list = new ArrayList<Integer>(); - создали ArrayList list = new ArrayList
+* list.add(1);                                                  list.add((Integer) 1);
+* int x = list.get(0);                                          int x = (Integer) list.get(0);
+* list.set(0,10);                                               list.set(0, (Integer) 10);
+* Эффект стирания типов информации.
+* */
